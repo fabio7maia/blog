78a004bc7ba17789671149a2e71610cbcf8fccb8
Fábio Maia <fabio.maia@itsector.pt>
2024-01-18 22:39:19 +0000

    fix: fixed deploy for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
-
-import netlify from "@astrojs/netlify/functions";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+// import vercel from "@astrojs/vercel/serverless";
+// import netlify from "@astrojs/netlify";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  site: "https://example.com",
+  site: "https://the-tech-blog.pages.dev",
   integrations: [mdx(), sitemap(), react()],
-  adapter: netlify()
+  adapter: cloudflare(), // vercel(), netlify()
 });
